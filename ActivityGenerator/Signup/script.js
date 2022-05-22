924e2ecafd1a8d05630d6807c83aa1c3d3e5aca4
Georgekou02 <106030936+Georgekou02@users.noreply.github.com>
2022-05-22 21:23:26 +0300

    Delete ActivityGenerator directory

--- ActivityGenerator/Signup/script.js
+++
@@ -1,7 +0,0 @@
-CaptchaImg = document.getElementById("CaptchaImg");
-
-function ChangeCaptcha()
-{
-    // Changes the captcha display text, input text and sets the session for captcha to the new captchaInputText
-    CaptchaImg.src = "Captcha/generateCaptcha.php";
-}
